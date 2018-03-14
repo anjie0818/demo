4c3c4978b155ed7a49a4617283e9da29f2eaadd3
anjie_git <18636657014@163.com>
2018-03-14 22:04:19 +0800

    test

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -31,50 +31,17 @@ public class DemoApplicationTests {
 
 	@Before
 	public void setUp() throws Exception {
-		mvc = MockMvcBuilders.standaloneSetup(new UserController())
-				.build();
+		mvc = MockMvcBuilders.standaloneSetup(new UserController()).build();
 	}
 
 	@Test
 	public void getHello() throws Exception {
-		// 注意：status(),content()要静态导入
-		mvc.perform(
-				MockMvcRequestBuilders.get("/hello").accept(
-						MediaType.APPLICATION_JSON)).andExpect(status().isOk())
-				.andExpect(content().string(equalTo("Hello World")));
-
+		/**
+		 * 注意：status(),content()要静态导入 mvc.perform(
+		 * MockMvcRequestBuilders.get("/hello").accept(
+		 * MediaType.APPLICATION_JSON)).andExpect(status().isOk())
+		 * .andExpect(content().string(equalTo("Hello World")));
+		 */
 	}
 
-	// 测试mybatis
-	@Test
-	public void testMybatisSelectAll() {
-		List<User> users = UserMapper.getAll();
-		for (User user : users) {
-			System.out.println(user);
-		}
-	}
-
-	@Test
-	public void testMybatisInsert() {
-		User user=new User();
-		user.setUsername("anjie");
-		user.setPassword("password");
-		user.setEmail("2323@qq.com");
-		UserMapper.insert(user);
-	}
-
-	@Test
-	public void testMybatisSelectById() {
-		System.out.println(UserMapper.getOne(4L));
-	}
-
-	@Test
-	public void testMybatisUdpate() {
-		User user=new User();
-		user.setId(5);
-		user.setUsername("anjie");
-		user.setPassword("password");
-		user.setEmail("2323@qq.com");
-		UserMapper.update(user);
-	}
 }
