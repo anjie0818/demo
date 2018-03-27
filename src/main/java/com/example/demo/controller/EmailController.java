cf795b1efb4a45e2cb04eadf0178702864f0d1aa
anjie <18636657014@163.com>
2018-03-27 23:14:48 +0800

    update file

--- src/main/java/com/example/demo/controller/EmailController.java
+++ src/main/java/com/example/demo/controller/EmailController.java
@@ -7,6 +7,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 public class EmailController {
@@ -17,12 +18,33 @@ public class EmailController {
 
         return "/email/index";
     }
+    @RequestMapping("/cemail/indexfile")
+    public String hellofile(){
+
+        return "/email/indexfile";
+    }
     @PostMapping("/cemail/post")
     public String postContent(@RequestParam(value = "subject") String subject,
                               @RequestParam(value = "to") String to,
                               @RequestParam(value = "content") String content, Model model){
         try {
             emailService.sendHtmlMail(to,subject,content);
+        } catch (Exception e) {
+            model.addAttribute("to","邮箱失败");
+            return "/email/success";
+        }
+        model.addAttribute("to",to);
+
+        return "/email/success";
+    }
+    @PostMapping("/cemail/postfile")
+    public String postContentFile(@RequestParam(value = "subject") String subject,
+                              @RequestParam(value = "to") String to,
+                              @RequestParam(value = "content") String content,
+                                  @RequestParam("file") MultipartFile file,
+                                  Model model){
+        try {
+            emailService.sendFileMail(to,subject,content,file);
         } catch (Exception e) {
             model.addAttribute("to","邮箱失败");
             return "/email/success";
--- src/main/java/com/example/demo/service/EmailService.java
+++ src/main/java/com/example/demo/service/EmailService.java
@@ -4,13 +4,19 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @Service
 public class EmailService  {
@@ -53,4 +59,29 @@ public class EmailService  {
     }
 
 
+    public void sendFileMail(String to, String subject, String content, MultipartFile file) throws Exception{
+
+        MimeMessage message = mailSender.createMimeMessage();
+
+        try {
+            MimeMessageHelper helper = new MimeMessageHelper(message, true);
+            helper.setFrom(from);
+            helper.setTo(to);
+            helper.setSubject(subject);
+            helper.setText(content, true);
+            //将file转为正式的file
+            // Get the file and save it somewhere
+            byte[] bytes = file.getBytes();
+            Path path = Paths.get("pic\\"+file.getOriginalFilename());
+            Files.write(path, bytes);
+            FileSystemResource fileSystemResource = new FileSystemResource(new File("pic\\"+file.getOriginalFilename()));
+            helper.addAttachment(file.getOriginalFilename(), fileSystemResource);
+
+            mailSender.send(message);
+            logger.info("带附件的邮件已经发送。");
+        } catch (Exception e) {
+            logger.error("发送带附件的邮件时发生异常！", e);
+            throw e;
+        }
+    }
 }
