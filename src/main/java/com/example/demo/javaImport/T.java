6c284ab12efd264973bf3710237e9b32f207d47f
jie an <18636657014@163.com>
2018-05-07 11:24:43 +0800

    5.7 update6

--- src/main/java/com/example/demo/javaImport/T.java
+++ src/main/java/com/example/demo/javaImport/T.java
@@ -3,6 +3,8 @@ package com.example.demo.javaImport;
 public class T
 {
     public static void main(String[] args) {
-        System.out.println("update5");
+        System.out.println("update6");
     }
 }
+
+
