71f84444b17535939c737a342a041c55295f039e
jie an <18636657014@163.com>
2018-05-07 11:08:14 +0800

    5.7 update4

--- src/main/java/com/example/demo/javaImport/T.java
+++ src/main/java/com/example/demo/javaImport/T.java
@@ -3,6 +3,6 @@ package com.example.demo.javaImport;
 public class T
 {
     public static void main(String[] args) {
-        System.out.println("update2");
+        System.out.println("update3");
     }
 }
